e8c20a63e59e357dd71c765c8e3bfa19e8cea7c1
Shota Fukumori (sora_h) <her@sorah.jp>
2014-06-24 00:30:03 +0900

    Look up from EC2 instances

--- main.go
+++ main.go
@@ -1,20 +1,35 @@
 package main
 
 import (
+	"os"
+	"net"
 	"fmt"
+	"strings"
 	"github.com/miekg/dns"
+	"github.com/mitchellh/goamz/aws"
+	"github.com/mitchellh/goamz/ec2"
 )
 
-func Serve(writer dns.ResponseWriter, req *dns.Msg) {
-	fmt.Println(req.Question[0].Name)
+type Handl struct {
+	Instances map[string]net.IP;
+}
+
+func (hn *Handl) ServeDNS(writer dns.ResponseWriter, req *dns.Msg) {
+	fmt.Println(strings.Split(req.Question[0].Name, ".")[0])
 
 	response := new(dns.Msg)
 	response.SetReply(req)
 	response.Authoritative = true
 
+	instance := hn.Instances[strings.Split(req.Question[0].Name, ".")[0]]
+	if instance == nil {
+		writer.WriteMsg(response)
+		return
+	}
+
 	rr := new(dns.A)
-	rr.Hdr = dns.RR_Header{Name: "test.internal.", Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 0}
-	rr.A = []byte{10,1,2,3}
+	rr.Hdr = dns.RR_Header{Name: req.Question[0].Name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 0}
+	rr.A = instance
 
 	switch req.Question[0].Qtype {
 	default:
@@ -30,8 +45,39 @@ func Serve(writer dns.ResponseWriter, req *dns.Msg) {
 func main() {
 	fmt.Println("Hello")
 
+	var regionName string
+	if regionName = os.Getenv("AWS_REGION"); regionName == "" {
+	  regionName = "ap-northeast-1"
+	}
+	region := aws.Regions[regionName]
+
+	auth, err := aws.EnvAuth()
+	if err != nil {
+		panic(err)
+	}
+
+	client := ec2.New(auth, region)
+	instances, err := client.Instances(nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	handler := new(Handl)
+	handler.Instances = make(map[string]net.IP, 10)
+
+	for _, reservation := range instances.Reservations {
+		for _, instance := range reservation.Instances {
+			for _, tag := range instance.Tags {
+				if tag.Key == "Name" {
+					fmt.Printf("%v -- %v\n", tag.Value, net.ParseIP(instance.PrivateIpAddress))
+					handler.Instances[tag.Value] = net.ParseIP(instance.PrivateIpAddress)
+				}
+			}
+		}
+	}
+
 	mux := dns.NewServeMux()
-	mux.HandleFunc(".", Serve)
+	mux.Handle(".", handler)
 
 	server := &dns.Server{
 		Addr: ":10053",
